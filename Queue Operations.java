56efa678589ae43330ffa4b12a05e6089d1a6954
rahimunshaik <88622657+rahimunshaik@users.noreply.github.com>
2023-06-17 09:56:46 +0530

    Create Queue Operations.java

---
+++ Queue Operations.java
@@ -0,0 +1,102 @@
+Given N integers, the task is to insert those elements in the queue. Also, given M integers, the task is to find the frequency of each number in the Queue.
+Note:
+
+insert() will be called N times by main().
+findFrequency() will be called M times by main();
+Where k is each element passing through respective function calls.
+Example 1:
+
+Input:
+N = 8
+1 2 3 4 5 2 3 1
+M = 5
+1 3 2 9 10
+Output:
+2
+2
+2
+-1
+-1
+Explanation:
+After inserting 1, 2, 3, 4, 5, 2, 3 and 1 into the queue, frequency of 1 is 2, 3 is 2 and 2 is 2. Since 9 and 10 are not there in the queue we output -1 for them.
+  //{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+// Helper class Geeks to implement 
+// insert() and findFrequency()
+class Geeks{
+    
+    // Function to insert element into the queue
+    static void insert(Queue<Integer> q, int k){
+        
+        // Your code here
+        q.offer(k);
+        
+    }
+    
+    // Function to find frequency of an element
+    // rteturn the frequency of k
+    static int findFrequency(Queue<Integer> q, int k){
+        
+        // Your code here
+        int size=q.size();
+        int count =0;
+        for(int i=0; i<size; i++)
+        {
+            int temp=q.poll();
+            if(temp == k) count++;
+            q.add(temp);
+        }
+        return (count ==0 ) ? -1 : count;
+        
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+// Driver class with driver code
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) {
+		
+		// Taking input using scanner class
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    // Declaring Queue
+		    Queue<Integer> q = new LinkedList<>();
+		    int n = sc.nextInt();
+		    
+		    // Invoking object of Geeks class
+		    Geeks obj = new Geeks();
+		    
+		    for(int i = 0;i<n;i++){
+		        int k = sc.nextInt();
+		        obj.insert(q, k);
+		    }
+		   
+		    int x = sc.nextInt();
+		    for(int i = 0;i<x;i++){
+		        int k = sc.nextInt();
+		        int f = obj.findFrequency(q, k);
+		        if(f != 0){
+		            System.out.println(f);
+		        }
+		        else{
+		            System.out.println("-1");
+		        }
+		    }
+		}
+	}
+}
+// } Driver Code Ends
