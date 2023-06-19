8db7a5b99f299564d133b567ece08dd181a875d8
rahimunshaik <88622657+rahimunshaik@users.noreply.github.com>
2023-06-19 14:22:18 +0530

    Create Rearrange an array with O(1) extra space June 19th2023.java

---
+++ Rearrange an array with O(1) extra space June 19th2023.java
@@ -0,0 +1,42 @@
+Given an array arr[] of size N where every element is in the range from 0 to n-1. Rearrange the given array so that the transformed array arrT[i] becomes arr[arr[i]].
+
+NOTE: arr and arrT are both same variables, representing the array before and after transformation respectively.
+
+Example 1:
+
+Input:
+N = 2
+arr[] = {1,0}
+Output: 0 1
+Explanation: 
+arr[arr[0]] = arr[1] = 0.
+arr[arr[1]] = arr[0] = 1.
+Example 2:
+
+Input:
+N = 5
+arr[] = {4,0,2,1,3}
+Output: 3 4 2 0 1
+Explanation: 
+arr[arr[0]] = arr[4] = 3.
+arr[arr[1]] = arr[0] = 4.
+and so on.
+================================================
+
+class Solution
+{
+ 
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    static void arrange(long arr[], int n)
+    {
+        // your code here
+        long[] a=arr.clone();
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=a[(int)a[i]];
+        }
+    }
+}
